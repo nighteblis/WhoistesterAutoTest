3a2380d35be10c5c00c79b41382e1f0350f9003a
nighteblis <nighteblis@hotmail.com>
2015-08-11 23:39:48 +0800

    complete the scan feature , scan all the class of the annotation of under working directory

--- src/cf/lihao/ClassScanner.java
+++ src/cf/lihao/ClassScanner.java
@@ -13,6 +13,8 @@ import java.util.Arrays;
 import cf.lihao.testlib.TestLibAnnotation;
 
 public class ClassScanner {
+	
+	private static File RunningLocation; 
 
 	public void Scanner() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 
@@ -20,40 +22,7 @@ public class ClassScanner {
 		System.out.println("Current dir:" + current);
 		String currentDir = System.getProperty("user.dir");
 		System.out.println("Current dir using System:" + currentDir);
-
-		File f = null;
-		File[] paths;
-
-		try {
-			// create new file
-			f = new File(currentDir);
-
-			// returns pathnames for files and directory
-			paths = f.listFiles();
-
-			// for each pathname in pathname array
-			for (File path : paths) {
-				if (path.toString().endsWith(".jar")
-						|| path.toString().endsWith(".class"))
-					// prints file and directory paths
-					System.out.println(path);
-
-			}
-
-			String[] directories = f.list(new FilenameFilter() {
-				public boolean accept(File current, String name) {
-					return new File(current, name).isDirectory();
-				}
-			});
-			System.out.println(Arrays.toString(directories));
-
-		} catch (Exception e) {
-			// if any error occurs
-			e.printStackTrace();
-		}
-
 		ArrayList<File> a = new ArrayList<File>();
-
 		listf(currentDir, a);
 
 	}
@@ -65,10 +34,11 @@ public class ClassScanner {
 		File[] fList = directory.listFiles();
 		for (File file : fList) {
 			if (file.isFile()) {
+			//	if (file.getName().contains("$")) continue;
 				if (/* file.getName().endsWith(".jar") || */file.getName()
 						.endsWith(".class")) {
 					files.add(file);
-					System.out.println(file.getParent());
+					System.out.println(file.getPath());
 					processclass(file);
 					
 				}
@@ -80,36 +50,50 @@ public class ClassScanner {
 
 	public void processclass(File file) throws MalformedURLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 
+		String filepath = file.getParent();
+	    String runninglocationString = this.RunningLocation.toString();
+	    int length = runninglocationString.length();
+	     String packageString = filepath.substring(length+1); 
+	    System.out.println("package "+packageString);
+	    packageString = packageString.replace("/", ".").replace("\\", ".");
+	    
+	    
 		URL dirUrl = new URL("file:/"+file.getParent()); // 1
 		URLClassLoader cl = new URLClassLoader(new URL[] { dirUrl }, this
 				.getClass().getClassLoader()); // 2
-		Class loadedClass = cl.loadClass("cf.lihao.util."+ file.getName().substring(0, file.getName().length() - 6));
+		System.out.println(packageString+"."+ file.getName().substring(0, file.getName().length() - 6));
+		Class loadedClass = cl.loadClass(packageString+"."+ file.getName().substring(0, file.getName().length() - 6));
+		
+		try {
 		Object obj = loadedClass.newInstance();
 
+		
 		// AnnotationRunner runner = new AnnotationRunner();
 		Method[] methods = obj.getClass().getMethods();
 
 		for (Method method : methods) {
 			TestLibAnnotation annos = method
 					.getAnnotation(TestLibAnnotation.class);
+			System.out.println("======method===="+method.getName());
 			if (annos != null) {
-				try {
+	
 					System.out
 							.println("got the annotation " + method.getName());
 					// method.invoke(obj);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+
 			}
+		}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 	}
 
 	public static void main(String[] para) {
 		
-		File location = new java.io.File(ClassScanner.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+		RunningLocation = new java.io.File(ClassScanner.class.getProtectionDomain().getCodeSource().getLocation().getPath());
 		
-		System.out.println(location);
+		System.out.println(RunningLocation);
 
 
 			try {
@@ -128,6 +112,14 @@ public class ClassScanner {
 				e.printStackTrace();
 			}
 
+	}
+
+	public File getRunningLocation() {
+		return RunningLocation;
+	}
+
+	public void setRunningLocation(File runningLocation) {
+		RunningLocation = runningLocation;
 	}
 
 }
